d13abc5b074020d20276ce0b3f2ac01d3f55b8be
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-08-04 17:17:22 -0500

    Integrar Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a Developer based in Peru!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box>
+          <Heading as="h2" variant="page-title">
+            Edson Ever David
+          </Heading>
+          <Heading as="h2" variant="page-title">
+            Quispe Salas
+          </Heading>
+          <p>
+            Developer<br></br>
+          </p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
