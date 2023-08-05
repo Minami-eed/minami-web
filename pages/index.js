ef5bb0a3d7000f139c386897fa782a7473c3b380
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-08-04 23:05:36 -0500

    Añadiendo transiciones con Framer Motion

--- pages/index.js
+++ pages/index.js
@@ -13,178 +13,185 @@ import {
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import { BioSection, BioYear } from '../components/bio'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 import Image from 'next/image'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        mt={3}
-        align="center"
-      >
-        Hello, I&apos;m a Developer based in Peru!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box>
-          <Heading as="h2" variant="page-title">
-            Edson Ever David
-          </Heading>
-          <Heading as="h2" variant="page-title">
-            Quispe Salas
-          </Heading>
-          <p>
-            Developer<br></br>
-          </p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          mt={3}
           align="center"
         >
+          Hello, I&apos;m a Developer based in Peru!
+        </Box>
+
+        <Box display={{ md: 'flex' }}>
+          <Box>
+            <Heading as="h2" variant="page-title">
+              Edson Ever David
+            </Heading>
+            <Heading as="h2" variant="page-title">
+              Quispe Salas
+            </Heading>
+            <p>
+              Developer<br></br>
+            </p>
+          </Box>
           <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
           >
-            <Image
-              src="/images/minami.png"
-              alt="Minami Profile image"
+            <Box
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              w="100px"
+              h="100px"
+              display="inline-block"
               borderRadius="full"
-              width="100"
-              height="100"
-            />
+              overflow="hidden"
+            >
+              <Image
+                src="/images/minami.png"
+                alt="Minami Profile image"
+                borderRadius="full"
+                width="100"
+                height="100"
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          I am a passionate and skilled developer specialized in software
-          development with experience in creating web and mobile applications.
-          <br></br>
-          <br></br>I possess knowledge of programming languages such as HTML,
-          CSS, JavaScript, Java, Kotlin, and am familiar with various
-          development technologies including databases, frameworks, and version
-          control tools. Additionally, I have experience in user interface
-          design, user experience optimization, and the use of ChatGPT.<br></br>
-          <br></br>
-          Having worked on software development projects in diverse
-          organizations, I have demonstrated exceptional ability to work
-          collaboratively and efficiently solve complex problems. I am an
-          effective communicator and take pride in my ability to translate user
-          requirements into effective technological solutions.<br></br>
-          <br></br>
-          Furthermore, I am committed to continuous education and am always
-          seeking to learn new skills and technologies to enhance my developer
-          skills. I am excited about the opportunity to further develop my
-          career as a developer and contribute my experience and skills to a
-          dynamic and creative team. Furthermore, if you would like to view my
-          Works, you may refer to my portfolio below.<br></br>
-          <br></br>
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button
-              rightIcon={<ChevronRightIcon />}
-              style={{ backgroundColor: '#805ad5', color: '#FFFFFF' }}
-            >
-              My Portfolio
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Biography
-        </Heading>
-
-        <BioSection>
-          <BioYear>2001</BioYear>
-          Born in La Libertad, Perú.
-        </BioSection>
-
-        <BioSection>
-          <BioYear>2023</BioYear>
-          He completed the degree in Computer Science and Informatics at the
-          Higher Education Institute in Trujillo, CIBERTEC.
-        </BioSection>
-
-        <BioSection>
-          <BioYear>2023 to present</BioYear>
-          Working as a freelancer.
-        </BioSection>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Recreational pursuits
-        </Heading>
-
-        <Paragraph>
-          Research, Music, Playing Piano, Video Games, Photography, Machine
-          Learning.
-        </Paragraph>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          In the digital realm
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/Minami-eed" target="_blank">
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            I am a passionate and skilled developer specialized in software
+            development with experience in creating web and mobile applications.
+            <br></br>
+            <br></br>I possess knowledge of programming languages such as HTML,
+            CSS, JavaScript, Java, Kotlin, and am familiar with various
+            development technologies including databases, frameworks, and
+            version control tools. Additionally, I have experience in user
+            interface design, user experience optimization, and the use of
+            ChatGPT.<br></br>
+            <br></br>
+            Having worked on software development projects in diverse
+            organizations, I have demonstrated exceptional ability to work
+            collaboratively and efficiently solve complex problems. I am an
+            effective communicator and take pride in my ability to translate
+            user requirements into effective technological solutions.<br></br>
+            <br></br>
+            Furthermore, I am committed to continuous education and am always
+            seeking to learn new skills and technologies to enhance my developer
+            skills. I am excited about the opportunity to further develop my
+            career as a developer and contribute my experience and skills to a
+            dynamic and creative team. Furthermore, if you would like to view my
+            Works, you may refer to my portfolio below.<br></br>
+            <br></br>
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
               <Button
-                variant="ghost"
-                style={{ color: '#805ad5' }}
-                leftIcon={<Icon as={IoLogoGithub} />}
+                rightIcon={<ChevronRightIcon />}
+                style={{ backgroundColor: '#805ad5', color: '#FFFFFF' }}
               >
-                @Minami-eed
+                My Portfolio
               </Button>
-            </Link>
-          </ListItem>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Biography
+          </Heading>
 
-          <ListItem>
-            <Link href="https://twitter.com/minami_eed" target="_blank">
-              <Button
-                variant="ghost"
-                style={{ color: '#805ad5' }}
-                leftIcon={<Icon as={IoLogoTwitter} />}
-              >
-                @Minami_eed
-              </Button>
-            </Link>
-          </ListItem>
+          <BioSection>
+            <BioYear>2001</BioYear>
+            Born in La Libertad, Perú.
+          </BioSection>
 
-          <ListItem>
-            <Link href="https://www.instagram.com/minami.eed/" target="_blank">
-              <Button
-                variant="ghost"
-                style={{ color: '#805ad5' }}
-                leftIcon={<Icon as={IoLogoInstagram} />}
+          <BioSection>
+            <BioYear>2023</BioYear>
+            He completed the degree in Computer Science and Informatics at the
+            Higher Education Institute in Trujillo, CIBERTEC.
+          </BioSection>
+
+          <BioSection>
+            <BioYear>2023 to present</BioYear>
+            Working as a freelancer.
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Recreational pursuits
+          </Heading>
+
+          <Paragraph>
+            Research, Music, Playing Piano, Video Games, Photography, Machine
+            Learning.
+          </Paragraph>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            In the digital realm
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://github.com/Minami-eed" target="_blank">
+                <Button
+                  variant="ghost"
+                  style={{ color: '#805ad5' }}
+                  leftIcon={<Icon as={IoLogoGithub} />}
+                >
+                  @Minami-eed
+                </Button>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link href="https://twitter.com/minami_eed" target="_blank">
+                <Button
+                  variant="ghost"
+                  style={{ color: '#805ad5' }}
+                  leftIcon={<Icon as={IoLogoTwitter} />}
+                >
+                  @Minami_eed
+                </Button>
+              </Link>
+            </ListItem>
+
+            <ListItem>
+              <Link
+                href="https://www.instagram.com/minami.eed/"
+                target="_blank"
               >
-                @Minami.eed
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
-      </Section>
-    </Container>
+                <Button
+                  variant="ghost"
+                  style={{ color: '#805ad5' }}
+                  leftIcon={<Icon as={IoLogoInstagram} />}
+                >
+                  @Minami.eed
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
