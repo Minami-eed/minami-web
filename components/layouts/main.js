a234b18ae1eaeab1afa312fa3bcfeb5e93634874
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-12-04 23:09:15 -0500

    Corrección en Main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,8 +8,8 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Minami homepage" />
-        <meta name="author" content="Edson Ever David Quispe Salas" />
+        <meta name="description" content="Minami website" />
+        <meta name="author" content="Edson, Quispe Salas" />
         <meta name="author" content="Minami EED" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
@@ -17,7 +17,7 @@ const Main = ({ children, router }) => {
         <meta name="twitter:site" content="@minami_eed" />
         <meta name="twitter:creator" content="@minami_eed" />
         <meta name="twitter:image" content="https://www.minami.bio/card.png" />
-        <meta property="og:site_name" content="Edson Ever David Website" />
+        <meta property="og:site_name" content="Minami EED" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.minami.bio/card.png" />
         <title>Home / Minami EED</title>
